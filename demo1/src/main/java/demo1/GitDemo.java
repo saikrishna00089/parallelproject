1e026bccee8808563a45f6ea81b463b40a324a78
sailinga <sailinga@DIN73003065.corp.capgemini.com>
2018-11-23 14:14:17 +0530

    first commit statement

---
+++ demo1/src/main/java/demo1/GitDemo.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class GitDemo {
+	public static void main(String[] args) {
+		System.out.println("hi welcome ");
+	}
+	public static void greeting() {
+		System.out.println("welcome to git");
+	}
+
+}
